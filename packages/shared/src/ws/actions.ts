7aac7d76e181ac8cf1e3f75a9e83328918fec25a
Denys Savytskyi <deonissavytsk@gmail.com>
2024-08-11 13:01:06 +0200

    readable ws action enums

--- packages/shared/src/ws/actions.ts
+++ packages/shared/src/ws/actions.ts
@@ -1,27 +1,27 @@
 // actions sent by client
-export const enum ClientAction {
-  NICKNAME = 0,
+export enum ClientAction {
+  NICKNAME = 'NICKNAME',
 
-  MOVE_ACTOR,
+  MOVE_ACTOR = 'CLIENT_MOVE_ACTOR',
 
-  PICK_ITEM,
-  CURSOR,
-  PICK_ACTOR,
-  RELEASE_ACTOR,
+  PICK_ITEM = 'PICK_ITEM',
+  CURSOR = 'CURSOR',
+  PICK_ACTOR = 'PICK_ACTOR',
+  RELEASE_ACTOR = 'RELEASE_ACTOR',
 }
 
 // actions sent by server
-export const enum ServerAction {
-  CLIENT_ID = ClientAction.RELEASE_ACTOR + 1,
-  STATE,
-  DOWNLOAD_PROGRESS,
+export enum ServerAction {
+  CLIENT_ID = 'CLIENT_ID',
+  STATE = 'STATE',
+  DOWNLOAD_PROGRESS = 'DOWNLOAD_PROGRESS',
 
-  MOVE_ACTOR,
+  MOVE_ACTOR = 'SERVER_MOVE_ACTOR',
 
-  CURSORS,
-  SPAWN_ACTOR, // spawnig absoultelly new actor
-  DROP_ACTOR, // spawning actor that is present in original state save. e.g. dropping item deck / state
-  REMOVE_ACTOR,
+  CURSORS = 'CURSORS',
+  SPAWN_ACTOR = 'SPAWN_ACTOR', // spawnig absoultelly new actor
+  DROP_ACTOR = 'DROP_ACTOR', // spawning actor that is present in original state save. e.g. dropping item deck / state
+  REMOVE_ACTOR = 'REMOVE_ACTOR',
 }
 
 export type SimAction = ClientAction | ServerAction;
