bea03445c1b0f039de3a7016a6f11454721dd8e4
Denys Savytskyi <deonissavytsk@gmail.com>
2024-09-12 15:16:58 +0200

    fix nest circular import

--- packages/server/src/users/users.module.ts
+++ packages/server/src/users/users.module.ts
@@ -1,19 +1,11 @@
-import { DynamicModule, Module } from '@nestjs/common';
-import { AuthModule } from '../auth/auth.module';
+import { forwardRef, Module } from '@nestjs/common';
 import { CaslAbilityFactory } from '../casl/casl-ability.factory';
 import { PermissionsService } from '../permissions.service';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
-const getAuthModele = async (): Promise<DynamicModule> => {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve(AuthModule as unknown as DynamicModule);
-    }, 1000);
-  });
-};
 @Module({
-  imports: [getAuthModele()],
+  imports: [forwardRef(() => import('../auth/auth.module').then(m => m.AuthModule))],
   controllers: [UsersController],
   providers: [UsersService, PermissionsService, CaslAbilityFactory],
   exports: [UsersService],
