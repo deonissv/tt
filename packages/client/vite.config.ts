8a1f5614975fc8fdab4359284ae0cdd72c66fd73
Denys Savytskyi <deonissavytsk@gmail.com>
2024-09-24 13:39:38 +0200

    move client public folder to root

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,17 +1,28 @@
 import react from '@vitejs/plugin-react';
+import * as path from 'node:path';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  server: {
-    host: true,
-    port: 5500,
-  },
-  preview: {
-    host: true,
-    port: 5500,
-    open: false,
-  },
+export default defineConfig(({ mode, isPreview }) => {
+  const isProduction = mode === 'production';
+
+  return {
+    plugins: [react(), tsconfigPaths()],
+    publicDir: !isProduction || isPreview ? path.resolve(__dirname, '..', '..', 'assets') : false,
+    build: {
+      outDir: path.resolve(__dirname, '..', '..', 'public'),
+      assetsDir: '.',
+      emptyOutDir: true,
+    },
+    server: {
+      host: true,
+      port: 5500,
+    },
+    preview: {
+      host: true,
+      port: 5500,
+      open: false,
+    },
+  };
 });
